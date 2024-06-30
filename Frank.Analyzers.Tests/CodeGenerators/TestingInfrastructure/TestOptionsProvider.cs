9a505b95e0840b771c45bb7d70eb2f227ca929ac
Frank R. Haugen <frank.haugen@gmail.com>
2024-06-30 18:45:41 +0200

    Add source generator for including additional files
    This update includes a source generator that adds additional files to the compilation. It also includes a code length analyzer that checks if the number of lines of code exceeds a maximum limit. Comprehensive tests have been written to verify the functionality of the source generator and code length analyzer.

---
+++ Frank.Analyzers.Tests/CodeGenerators/TestingInfrastructure/TestOptionsProvider.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Frank.SourceGenerator.AdditionalFiles.Tests;
+
+internal class TestOptionsProvider : AnalyzerConfigOptionsProvider
+{
+    private readonly TestAnalyzerConfigOptions _options = new();
+
+    public TestOptionsProvider()
+    {
+        _options.Add("build_property.rootnamespace", "Frank.GameEngine.Assets");
+        _options.Add("build_property.projectdir", @"C:\repos\frankhaugen\Frank.GameEngine\src\Frank.GameEngine.Assets");
+    }
+
+    public override AnalyzerConfigOptions GlobalOptions => _options;
+
+    public override AnalyzerConfigOptions GetOptions(SyntaxTree tree) => _options;
+
+    public override AnalyzerConfigOptions GetOptions(AdditionalText textFile) => _options;
+}
---
+++ Frank.Analyzers.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace Frank.Analyzers.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
