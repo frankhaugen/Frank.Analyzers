0d2fc303c9da1ddb5a17f170dc5f51e5b8a8dd5c
Frank R. Haugen <frank.haugen@gmail.com>
2024-07-08 15:50:28 +0200

    Update string formatting in test code
    The string formatting in the AdditionalFilesHelperGeneratorTests class in Frank.Analyzers.Tests has been modified. Specifically, a command to replace line endings has been added. This ensures consistent test results across different environments.

--- Frank.Analyzers.Tests/CodeGenerators/AdditionalFilesHelperGeneratorTests.cs
+++ Frank.Analyzers.Tests/CodeGenerators/AdditionalFilesHelperGeneratorTests.cs
@@ -62,7 +62,7 @@ public class AdditionalFilesHelperGeneratorTests : CSharpSourceGeneratorTest<Add
                     }
                 }
             }
-            """,
+            """.ReplaceLineEndings(),
             Encoding.UTF8
             )));
         
